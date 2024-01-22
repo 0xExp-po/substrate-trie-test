9502c635556316fc027a973ca3c790a962ab55fc
lucas-po <lucaschang1127@gmail.com>
2024-01-22 12:30:16 -0300

    Add dump file

--- src/substrate_trie_test.rs
+++ src/substrate_trie_test.rs
@@ -2,44 +2,44 @@
 
 use hex_literal::hex;
 use sp_core::{Blake2Hasher, H256};
-use sp_trie::{MemoryDB, LayoutV1, trie_types::TrieDBMutBuilderV1, TrieMut, TrieHash, TrieDBMut};
+use sp_trie::{trie_types::TrieDBMutBuilderV1, LayoutV1, MemoryDB, TrieDBMut, TrieHash, TrieMut};
 
 #[test]
 pub fn root() {
     let mut db = MemoryDB::new(&[0u8]);
     let mut root: TrieHash<LayoutV1<Blake2Hasher>> = Default::default();
 
-    let mut t: TrieDBMut<'_, LayoutV1<Blake2Hasher>> = TrieDBMutBuilderV1::new(&mut db, &mut root).build();
+    let mut t: TrieDBMut<'_, LayoutV1<Blake2Hasher>> =
+        TrieDBMutBuilderV1::new(&mut db, &mut root).build();
 
     let entries: Vec<(&[u8], &[u8])> = vec![
-		// "alfa" is at a hash-referenced leaf node.
-		(b"alfa", &[0; 40]),
-		// "bravo" is at an inline leaf node.
-		(b"bravo", b"bravo"),
-		// "do" is at a hash-referenced branch node.
-		(b"do", b"verb"),
-		// "dog" is at a hash-referenced branch node.
-		(b"dog", &[0; 40]),
-		// "doge" is at a hash-referenced leaf node.
-		(b"doge", &[0; 40]),
-		// extension node "o" (plus nibble) to next branch.
-		(b"horse", b"stallion"),
-		(b"house", b"building"),
-	];
+        // "alfa" is at a hash-referenced leaf node.
+        (b"alfa", &[0; 40]),
+        // "bravo" is at an inline leaf node.
+        (b"bravo", b"bravo"),
+        // "do" is at a hash-referenced branch node.
+        (b"do", b"verb"),
+        // "dog" is at a hash-referenced branch node.
+        (b"dog", &[0; 40]),
+        // "doge" is at a hash-referenced leaf node.
+        (b"doge", &[0; 40]),
+        // extension node "o" (plus nibble) to next branch.
+        (b"horse", b"stallion"),
+        (b"house", b"building"),
+    ];
 
     for entry in entries {
-       let _ = t.insert(entry.0, entry.1).expect("inserted");
+        let _ = t.insert(entry.0, entry.1).expect("inserted");
     }
 
     t.commit();
 
-	let root = &H256(hex!["f465577d5a13b1a48ea7245c559ab78175eb509b00829cf5db09d61ea795b74a"]);
+    let root = &H256(hex![
+        "f465577d5a13b1a48ea7245c559ab78175eb509b00829cf5db09d61ea795b74a"
+    ]);
     let actual: &TrieHash<LayoutV1<Blake2Hasher>> = t.root();
 
     println!("ROOT {:?}", actual);
 
-    assert_eq!(
-        actual,
-        root
-    );
+    assert_eq!(actual, root);
 }
